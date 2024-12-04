3180e8500917f67be203e9b0cf807d97947b8bf7
AConvert <angelo.convertini@boohoo.com>
2024-12-04 12:59:15 +0000

    updated fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,34 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const matter = localFont({
+  src: [
+    {
+      path: "./fonts/Matter-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Matter-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Matter-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Matter-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "./fonts/Matter-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Matter-Heavy.ttf",
+      weight: "800",
+    },
+  ],
+  variable: "--matter",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +44,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${matter.variable}`}>{children}</body>
     </html>
   );
 }
